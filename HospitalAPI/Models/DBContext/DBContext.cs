13285334824c652bdb2ab26d5c8336c7e726f682
A.K.M Mainul Hasan <88575577+mhasanlab@users.noreply.github.com>
2022-09-17 03:34:07 +0600

    hospital management system

---
+++ HospitalAPI/Models/DBContext/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HospitalAPI.Models.Class;
+namespace HospitalAPI.Models.DBContext
+{
+    public class DBContext
+    {
+        
+    }
+}
