b50909921e6815a630bb0dfa831ba6f39299db2d
Imam-masum <95578514+Imam-masum@users.noreply.github.com>
2022-09-17 23:30:08 +0600

    Update Appointment.cs

--- HospitalAPI/Models/Class/Appointment.cs
+++ HospitalAPI/Models/Class/Appointment.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace HospitalAPI.Models.Class
 {
     public class Appointment
     {
+         public int AppointmentId { get; set; }
+        public string Type { get; set; }
+        [Required, Column(TypeName = "date"), DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+
+        public DateTime AppointmentDate { get; set; }
+
+        [Required, Column(TypeName = "date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        public DateTime AppointmentTime { get; set; }
+
+        public string AppointmentDes { get; set; }
+
+        public int PatientId { get; set; }
+
+        public int DoctorId { get; set; }
+
+        //Nev
+        public virtual Doctor Doctor { get; set; }
+        public virtual Patient Patient { get; set; }
     }
 }
