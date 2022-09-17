cfa5052b7b051a8fda7810e7262f16cf66a3773a
Imam-masum <95578514+Imam-masum@users.noreply.github.com>
2022-09-17 23:39:16 +0600

    Update Doctor.cs

--- HospitalAPI/Models/Class/Doctor.cs
+++ HospitalAPI/Models/Class/Doctor.cs
@@ -7,5 +7,17 @@ namespace HospitalAPI.Models.Class
 {
     public class Doctor
     {
+         public int DoctorId { get; set; }
+        [Required,StringLength(100)]
+
+        public string FirstName { get; set; }
+
+        [Required, StringLength(100)]
+
+        public string LastName { get; set; }
+
+        public string Specialist { get; set; }
+        //nev
+        public virtual ICollection<Appointment> Appointments { get; set; }
     }
 }
