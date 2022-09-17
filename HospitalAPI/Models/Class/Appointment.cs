fa550b8c6c9ebc84848bce0a51dea3735a06d43f
Imam-masum <95578514+Imam-masum@users.noreply.github.com>
2022-09-17 23:36:49 +0600

    Update Appointment.cs

--- HospitalAPI/Models/Class/Appointment.cs
+++ HospitalAPI/Models/Class/Appointment.cs
@@ -7,5 +7,24 @@ namespace HospitalAPI.Models.Class
 {
     public class Appointment
     {
+        public int AppointmentId { get; set; }
+        public string Type { get; set; }
+        [Required, Column(TypeName = "date"), DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+
+        public DateTime AppointmentDate { get; set; }
+
+        [Required, Column(TypeName = "date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+
+        public DateTime AppointmentTime { get; set; }
+
+        public string AppointmentDes { get; set; }
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
+
+        //Nev
+        public virtual Doctor Doctor { get; set; }
+        public virtual Patient Patient { get; set; }
     }
 }
