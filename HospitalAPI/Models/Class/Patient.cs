4e30b69a82b32e49f40f52ede808dd3a923d36f7
Imam-masum <95578514+Imam-masum@users.noreply.github.com>
2022-09-17 23:40:23 +0600

    Update Patient.cs

--- HospitalAPI/Models/Class/Patient.cs
+++ HospitalAPI/Models/Class/Patient.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace HospitalAPI.Models.Class
 {
     public class Patient
     {
+        public int PatientId { get; set; }
+        [Required,StringLength(100)]
+        public string FirstName { get; set; }
+        [Required, StringLength(100)]
+        public string LastName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required, Column(TypeName = "date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string DOB { get; set; }
+        [Required,StringLength(12)]
+        public string Phone { get; set; }
+        [Required,StringLength(50)]
+        public string Email { get; set; }
+        //nev
+        public virtual ICollection<Appointment> Appointments { get; set; }
     }
 }
